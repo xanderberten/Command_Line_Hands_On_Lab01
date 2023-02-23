8a8f151f126109b97434e68b3376c1391e138f96
Xander <berten.xander@gmail.com>
2023-02-23 19:08:40 +0100

    Update

--- Command_Line_Hands_On_Lab01.cpp
+++ Command_Line_Hands_On_Lab01.cpp
@@ -25,7 +25,7 @@ enum class BlockType
 	glass = 3
 };
 
-enum class FacesNotToRender
+enum class FacesToRender
 {
 	top,
 	bottom,
@@ -40,7 +40,7 @@ struct Block
 	float3 position;
 	BlockType type;
 	bool isOpaque;
-	std::vector<FacesNotToRender> facesNotToRender;
+	std::vector<FacesToRender> facesToRender;
 };
 
 void SetInputAndOutputLocations(int argc, char** argv, std::string& input, std::string& output)
@@ -104,91 +104,91 @@ std::vector<Block> GetBlocksOfJson(const rapidjson::Value& jsonArray)
 }
 
 //Checks if there is a neighbor and
-////Sets a flag what faces not to render
+//Sets a flag what faces  to render
 void hasOpaqueNeighbors(Block& block, const std::vector<Block>& blocks)
 {
 	//Note: This will only work when the blocks are are on a uniform grid
 
-	if (block.isOpaque)
+	//if (!block.isOpaque)
 	{
 		//Check X Direction
 		if (std::find_if(blocks.begin(), blocks.end(), [&](const Block& otherBlock)
 			{
-				if (!otherBlock.isOpaque) return false;
-		return(
-			otherBlock.position.x == block.position.x + 1
-			&& otherBlock.position.y == block.position.y
-			&& otherBlock.position.z == block.position.z);
+				//if (!otherBlock.isOpaque) return true;
+				return(
+					otherBlock.position.x == block.position.x + 1
+					&& otherBlock.position.y == block.position.y
+					&& otherBlock.position.z == block.position.z);
 		//This is the actual if() check
 		//If the lambda function returns false for all elements in the vector, then std::find_if returns an iterator equal to blocks.end().
-		//So this will evaluate to true if not all elements return false
-		//In other words this evaluates to true if there is a neighbor
-			}) != blocks.end())
+		//So this will evaluate to false if not all elements return false (if there are no neighbors)
+		//In other words this evaluates to false if there is a neighbor
+			}) == blocks.end())
 		{
-			block.facesNotToRender.emplace_back(FacesNotToRender::right);
+			block.facesToRender.emplace_back(FacesToRender::right);
 		}
 
 
 			if (std::find_if(blocks.begin(), blocks.end(), [&](const Block& otherBlock)
 				{
-					if (!otherBlock.isOpaque) return false;
-			return(
-				otherBlock.position.x == block.position.x - 1
-				&& otherBlock.position.y == block.position.y
-				&& otherBlock.position.z == block.position.z);
-				}) != blocks.end())
+					//if (otherBlock.isOpaque) return false;
+					return(
+						otherBlock.position.x == block.position.x - 1
+						&& otherBlock.position.y == block.position.y
+						&& otherBlock.position.z == block.position.z);
+				}) == blocks.end())
 			{
-				block.facesNotToRender.emplace_back(FacesNotToRender::left);
+				block.facesToRender.emplace_back(FacesToRender::left);
 			}
 
 				//Check Y Direction
 				if (std::find_if(blocks.begin(), blocks.end(), [&](const Block& otherBlock)
 					{
-						if (!otherBlock.isOpaque) return false;
-				return(
-					otherBlock.position.x == block.position.x
-					&& otherBlock.position.y == block.position.y + 1
-					&& otherBlock.position.z == block.position.z);
-					}) != blocks.end())
+						//if (otherBlock.isOpaque) return false;
+						return(
+							otherBlock.position.x == block.position.x
+							&& otherBlock.position.y == block.position.y + 1
+							&& otherBlock.position.z == block.position.z);
+					}) == blocks.end())
 				{
-					block.facesNotToRender.emplace_back(FacesNotToRender::front);
+					block.facesToRender.emplace_back(FacesToRender::front);
 				}
 
 					if (std::find_if(blocks.begin(), blocks.end(), [&](const Block& otherBlock)
 						{
-							if (!otherBlock.isOpaque) return false;
-					return(
-						otherBlock.position.x == block.position.x
-						&& otherBlock.position.y == block.position.y - 1
-						&& otherBlock.position.z == block.position.z);
-						}) != blocks.end())
+							//if (otherBlock.isOpaque) return false;
+							return(
+								otherBlock.position.x == block.position.x
+								&& otherBlock.position.y == block.position.y - 1
+								&& otherBlock.position.z == block.position.z);
+						}) == blocks.end())
 					{
-						block.facesNotToRender.emplace_back(FacesNotToRender::back);
+						block.facesToRender.emplace_back(FacesToRender::back);
 					}
 
 						//Check Z Direction
 						if (std::find_if(blocks.begin(), blocks.end(), [&](const Block& otherBlock)
 							{
-								if (!otherBlock.isOpaque) return false;
-						return(
-							otherBlock.position.x == block.position.x
-							&& otherBlock.position.y == block.position.y
-							&& otherBlock.position.z == block.position.z + 1);
-							}) != blocks.end())
+								//if (otherBlock.isOpaque) return false;
+								return(
+									otherBlock.position.x == block.position.x
+									&& otherBlock.position.y == block.position.y
+									&& otherBlock.position.z == block.position.z + 1);
+							}) == blocks.end())
 						{
-							block.facesNotToRender.emplace_back(FacesNotToRender::top);
+							block.facesToRender.emplace_back(FacesToRender::top);
 						}
 
 							if (std::find_if(blocks.begin(), blocks.end(), [&](const Block& otherBlock)
 								{
-									if (!otherBlock.isOpaque) return false;
-							return(
-								otherBlock.position.x == block.position.x
-								&& otherBlock.position.y == block.position.y
-								&& otherBlock.position.z == block.position.z - 1);
-								}) != blocks.end())
+									//if (otherBlock.isOpaque) return false;
+									return(
+										otherBlock.position.x == block.position.x
+										&& otherBlock.position.y == block.position.y
+										&& otherBlock.position.z == block.position.z - 1);
+								}) == blocks.end())
 							{
-								block.facesNotToRender.emplace_back(FacesNotToRender::bottom);
+								block.facesToRender.emplace_back(FacesToRender::bottom);
 							}
 	}
 
@@ -242,53 +242,120 @@ int main(int argc, char** argv)
 		for (size_t i{}; i < blocks.size(); i++)
 		{
 			//Vertices Positions
-			fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z - 0.5f);
-			fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z + 0.5f);
-			fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y + 0.5f, blocks[i].position.z - 0.5f);
-			fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y + 0.5f, blocks[i].position.z + 0.5f);
-			fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x + 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z - 0.5f);
-			fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x + 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z + 0.5f);
-			fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x + 0.5f, blocks[i].position.y + 0.5f, blocks[i].position.z - 0.5f);
-			fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x + 0.5f, blocks[i].position.y + 0.5f, blocks[i].position.z + 0.5f);
+			//fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z - 0.5f);//Left		Back	Bottom
+			//fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z + 0.5f);//Left		Back	Top		
+			//fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y + 0.5f, blocks[i].position.z - 0.5f);//Left		Front	Bottom	
+			//fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y + 0.5f, blocks[i].position.z + 0.5f);//Left		Front	Top
+			//fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x + 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z - 0.5f);//Right		Back	Bottom
+			//fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x + 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z + 0.5f);//Right		Back	Top
+			//fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x + 0.5f, blocks[i].position.y + 0.5f, blocks[i].position.z - 0.5f);//Right		Front	Bottom
+			//fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x + 0.5f, blocks[i].position.y + 0.5f, blocks[i].position.z + 0.5f);//Right		Front	Top
 
-			int ammountOfFaces{};
-			for (size_t face{}; face < blocks[i].facesNotToRender.size(); face++)
+			int amountOfFaces{};
+			for (size_t face{}; face < blocks[i].facesToRender.size(); face++)
 			{
-				//if (blocks[i].facesNotToRender[face] )
-				switch (blocks[i].facesNotToRender[face])
+				switch (blocks[i].facesToRender[face])
 				{
-				case FacesNotToRender::top:
+				case FacesToRender::top:
 				{
-					fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z - 1.5f);//Down
-					fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y + 1.5f, blocks[i].position.z - 0.5f);//Front
-					fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y - 1.5f, blocks[i].position.z - 0.5f);//Back
-					fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n", blocks[i].position.x + 1.5f, blocks[i].position.y - 0.5f, blocks[i].position.z - 0.5f);//Right
-					fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n", blocks[i].position.x - 1.5f, blocks[i].position.y - 0.5f, blocks[i].position.z - 0.5f);//Left
+					fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z + 0.5f);//Left		Back	Top
+					fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y + 0.5f, blocks[i].position.z + 0.5f);//Left		Front	Top
+					fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x + 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z + 0.5f);//Right		Back	Top
+					fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x + 0.5f, blocks[i].position.y + 0.5f, blocks[i].position.z + 0.5f);//Right		Front	Top
 
+					fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z + 0.5f);//Up
+					//Up
+					fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + faceAddition, 2 + faceAddition, 7 + faceAddition, 2 + faceAddition, 5 + faceAddition, 2 + faceAddition);
+					fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + faceAddition, 2 + faceAddition, 3 + faceAddition, 2 + faceAddition, 7 + faceAddition, 2 + faceAddition);
+
+					amountOfFaces += 2;
+					break;
+				}
+				case FacesToRender::bottom:
+				{
+					fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z - 0.5f);//Left		Back	Bottom
+					fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y + 0.5f, blocks[i].position.z - 0.5f);//Left		Front	Bottom
+					fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x + 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z - 0.5f);//Right		Back	Bottom
+					fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x + 0.5f, blocks[i].position.y + 0.5f, blocks[i].position.z - 0.5f);//Right		Front	Bottom
+
+					fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z - 1.5f);//Down
 					//Down
 					fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + faceAddition, 6 + faceAddition, 4 + faceAddition, 6 + faceAddition, 3 + faceAddition, 6 + faceAddition);
 					fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + faceAddition, 6 + faceAddition, 2 + faceAddition, 6 + faceAddition, 4 + faceAddition, 6 + faceAddition);
-					//Front
-					fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 3 + faceAddition, 3 + faceAddition, 8 + faceAddition, 3 + faceAddition, 7 + faceAddition, 3 + faceAddition);
-					fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 3 + faceAddition, 3 + faceAddition, 4 + faceAddition, 3 + faceAddition, 8 + faceAddition, 3 + faceAddition);
-					//Back
-					fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 5 + faceAddition, 5 + faceAddition, 7 + faceAddition, 5 + faceAddition, 8 + faceAddition, 5 + faceAddition);
-					fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 5 + faceAddition, 5 + faceAddition, 8 + faceAddition, 5 + faceAddition, 6 + faceAddition, 5 + faceAddition);
-					//Right
-					fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + faceAddition, 4 + faceAddition, 5 + faceAddition, 4 + faceAddition, 6 + faceAddition, 4 + faceAddition);
-					fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + faceAddition, 4 + faceAddition, 6 + faceAddition, 4 + faceAddition, 2 + faceAddition, 4 + faceAddition);
-					//Left
-					fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 2 + faceAddition, 1 + faceAddition, 6 + faceAddition, 1 + faceAddition, 8 + faceAddition, 1 + faceAddition);
-					fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 2 + faceAddition, 1 + faceAddition, 8 + faceAddition, 1 + faceAddition, 4 + faceAddition, 1 + faceAddition);
 
-					ammountOfFaces += 7;
+					amountOfFaces += 2;
 					break;
 				}
-				case FacesNotToRender::bottom: {break; }
-				case FacesNotToRender::front: {break; }
-				case FacesNotToRender::back: {break; }
-				case FacesNotToRender::left: {break; }
-				case FacesNotToRender::right: {break; }
+				case FacesToRender::front:
+				{
+					fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y + 1.5f, blocks[i].position.z - 0.5f);//Front
+
+					if (std::find_if(blocks[i].facesToRender.begin(), blocks[i].facesToRender.end(), [&](const FacesToRender otherFace)
+						{
+							return otherFace == FacesToRender::top;
+						}) != blocks[i].facesToRender.end())
+					{
+						//When there is no top face that needs to be rendered
+						fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y + 0.5f, blocks[i].position.z + 0.5f);//Left		Front	Top
+						fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x + 0.5f, blocks[i].position.y + 0.5f, blocks[i].position.z + 0.5f);//Right		Front	Top
+
+						faceAddition -= 1;
+					}
+
+						if (std::find_if(blocks[i].facesToRender.begin(), blocks[i].facesToRender.end(), [&](const FacesToRender otherFace)
+							{
+								return otherFace == FacesToRender::bottom;
+							}) != blocks[i].facesToRender.end())
+						{
+							//When there is no bottom face that needs to be rendered
+							fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y + 0.5f, blocks[i].position.z - 0.5f);//Left		Front	Bottom	
+							fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x + 0.5f, blocks[i].position.y + 0.5f, blocks[i].position.z - 0.5f);//Right		Front	Bottom
+							faceAddition -= 1;
+						}
+
+							//Front
+							fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 3 + faceAddition, 3 + faceAddition, 8 + faceAddition, 3 + faceAddition, 7 + faceAddition, 3 + faceAddition);
+							fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 3 + faceAddition, 3 + faceAddition, 4 + faceAddition, 3 + faceAddition, 8 + faceAddition, 3 + faceAddition);
+
+							amountOfFaces += 2;
+							break;
+				}
+				case FacesToRender::back:
+				{
+					if (std::find_if(blocks[i].facesToRender.begin(), blocks[i].facesToRender.end(), [&](const FacesToRender otherFace)
+						{
+							return otherFace == FacesToRender::top;
+						}) != blocks[i].facesToRender.end())
+					{
+						//When there is no top face that needs to be rendered
+						fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z + 0.5f);//Left		Back	Top
+						fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x + 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z + 0.5f);//Right		Back	Top
+					}
+
+						if (std::find_if(blocks[i].facesToRender.begin(), blocks[i].facesToRender.end(), [&](const FacesToRender otherFace)
+							{
+								return otherFace == FacesToRender::bottom;
+							}) != blocks[i].facesToRender.end())
+						{
+							//When there is no bottom face that needs to be rendered
+							fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z - 0.5f);//Left		Back	Bottom
+							fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", blocks[i].position.x + 0.5f, blocks[i].position.y - 0.5f, blocks[i].position.z - 0.5f);//Right		Back	Bottom
+						}
+
+							fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n", blocks[i].position.x - 0.5f, blocks[i].position.y - 1.5f, blocks[i].position.z - 0.5f);//Back
+
+							//Back
+							fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 5 + faceAddition, 5 + faceAddition, 7 + faceAddition, 5 + faceAddition, 8 + faceAddition, 5 + faceAddition);
+							fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 5 + faceAddition, 5 + faceAddition, 8 + faceAddition, 5 + faceAddition, 6 + faceAddition, 5 + faceAddition);
+
+							amountOfFaces += 2;
+							break;
+				}
+				case FacesToRender::left:
+				{
+
+				}
+				case FacesToRender::right: {break; }
 
 				}
 
@@ -322,7 +389,7 @@ int main(int argc, char** argv)
 				//fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 2 + faceAddition, 1 + faceAddition, 8 + faceAddition, 1 + faceAddition, 4 + faceAddition, 1 + faceAddition);
 
 				//ammountOfFaces += 8;
-				faceAddition += ammountOfFaces;
+				faceAddition += amountOfFaces;
 			}
 		}
 
